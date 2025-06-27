d36b9e71715627708a9fe8a69ddb203569e1fc99
YosuaCH <160356610+YosuaCH@users.noreply.github.com>
2025-06-28 01:20:12 +0700

    Update progressBar.js

--- js/progressBar.js
+++ js/progressBar.js
@@ -5,20 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
   function animateProgressBar() {
     const isMobileView = window.innerWidth <= 768;
 
-    // Reset animasi
     progressBar.style.transition = "none";
     progressBar.style.width = "0%";
     progressBar.style.transformOrigin = isMobileView ? "center" : "left";
 
-    // Paksa browser render ulang
     void progressBar.offsetWidth;
 
-    // Jalankan animasi
     progressBar.style.transition = "width 1.5s ease-out";
     progressBar.style.width = "100%";
   }
 
-  // Observer unik
   const progressObserver = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
@@ -28,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     },
     {
-      threshold: 0.6, // elemen harus terlihat minimal 60%
+      threshold: 0.6,
     }
   );
 
